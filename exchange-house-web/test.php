f33909fc55b64a36072913a72ede2817b0a6d14e
Administrator <Administrator@PC-201710191503>
2017-11-26 09:07:03 +0800

    -test

--- exchange-house-web/test.php
+++ exchange-house-web/test.php
@@ -1,16 +1,3 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of test
- *
- * @author mr.c.en
- */
-class test {
-    //put your code here
-}
+echo 'hello';
